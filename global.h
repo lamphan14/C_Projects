cf7808264afd79e9019a6f67c98599b8d781eefe
Lam Phan <ltphan14@gmail.com>
2022-06-24 22:39:51 -0500

    Add files via upload

---
+++ global.h
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+
+/*This file contains all the values that is nesscessary for this prject
+This file will be upgarded if needed for all the calculation*/
+
+
+//Critical density//
+double Critical_Density = pow(10,-26);
+double km = 1 *pow (10,3); //kilometer
+double ly = 1 *pow (10,13); //light year
