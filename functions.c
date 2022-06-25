caec6a321c4be16d9377acc8bb37418accb9178a
Lam Phan <ltphan14@gmail.com>
2022-06-24 23:26:33 -0500

    Update functions.c
    updated the brightness of the star

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include "functions.h"
+#include "global.h"
+
+/*
+*This function will calculate the star brightness
+*defined as the power crossing unit area at the Earth perpendicular to the path of the light
+*Luminosity = L is the total power radiated in watts
+*Apparent brightness b
+*d is the distance from the star to Earth
+* b = L/(4*pi*d^2)
+*/
+void Star_Brightness_Calc(double L, double d)
+{
+    double b; //brightness of the star
+    b = L/(4*pi*pow(d,2);
+}
