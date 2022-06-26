c430a1020fbfe3cab3c241303ce7dadbbd6b3a70
Lam Phan <ltphan14@gmail.com>
2022-06-25 19:40:46 -0500

    added test code and change float to doulbe

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-#include "KineticEngergy.c"
-#include "global.h"
+#include <stdlib.h>
+#include "KineticEnergy.h"
 int main()
 {
-
-    return 0;
+double a= 30.5;
+double b = 50.0;
+double t2 = 40.0;
+double t1 = 30.0;
+double displacement;
+double timeElapsed;
+double averageVelocity;
+displacement = getDisplacement(a,b);
+timeElapsed = getTimeElapsed(t2,t1);
+averageVelocity = getAverageVelocity(displacement, timeElapsed);
+return 0;
 }
